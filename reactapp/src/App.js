35b1fc84f0450d67c1a940a675e400b5057d9986
Iamneo <techadmin@iamneo.ai>
2023-08-03 05:24:19 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Stopwatch from './components/Stopwatch/Stopwatch';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <h1>Stopwatch Application</h1>
-      <Stopwatch />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
